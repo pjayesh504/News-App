c1a47d9d015749b86a8fa8343d7d3e220cd32cc6
Jayesh504 <pjayesh504@gmail.com>
2022-12-02 22:55:04 +0530

    First commit

---
+++ src/components/News.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=97d771e0ad0b4adb880c50036c88d189&page=1&pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    }
+
+    handlePrevClick = async () => {
+        console.log("Privious")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=97d771e0ad0b4adb880c50036c88d189&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("Next")
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=97d771e0ad0b4adb880c50036c88d189&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+
+
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h2>NewsMonkey - Top Headlines </h2>
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Privious</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl} = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl?imageUrl:"https://images.moneycontrol.com/static-mcnews/2020/07/petroldieselpetrol-pumpfuel-station-770x433.jpg"} alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
